02c1a26e84adad0608b231ba0898699e7008a733
Byron Niblett <bniblett@gmail.com>
2019-08-19 16:02:04 -0400

    Mysql Backup Script

--- php/mysql-backup.php
+++ php/mysql-backup.php
@@ -1,27 +1,37 @@
 <?php
   /*
-   * Backup Options
-   *
+   * Archiving Variables
    */
-  $tmpDir = "/home/php/temp_dir/";        // Location of your temp directory
-  $user = "DB_USER";                      // Usernamefor database
-  $password = "DB_PASSWORD";              // Password for database
-  $dbName = "DB_NAME";    // Database name to backup
-  $dbHost = "DB_HOST";    // Hostname or IP address where database resides (localhost or xxx.xxx.xxx.xxx)
-  $prefix = "db_"; // The compressed file will have this prefix
+  define( "TMP_DIR", "TMP_DIR" ); # Temporary File Path to save files (Trailing Slash Required)
+  define( "DB_USER", "DB_USER" ); # DB User
+  define( "DB_PASS", "DB_PASS" ); # DB Pass
+  define( "DB_NAME", "DB_NAME" ); # DB Name
+  define( "DB_HOST", "DB_HOST" ); # DB Host
+  define( "FPREFIX", "FPREFIX" ); # File Prefix
 
-  // Create the database backup file
-  $sqlFile = $tmpDir.$prefix.date('Y_m_d_H:i:s').".sql";  // Name of the .sql file we create for the database
-  $backupFilename = $prefix.date('Y_m_d_H:i:s').".tgz";   // Name of the .tgz backup file we upload to Dropbox
-  $backupFile = $tmpDir.$backupFilename;  // Path to the backup file in the temp directory
+  /*
+   * Create File Names
+   * One for .sql, other .tgz
+   */
+  $filename = FPREFIX . date('Y-m-d-H:i:s'); # database dump file
+  $file_sql = $filename . ".sql";
+  $file_tgz = $filename . ".tgz";
 
-  // Shell commands for generating the .sql and .tgz files
-  $createBackup = "mysqldump -h ".$dbHost." -u ".$user." --password=".$password." ".$dbName." --> ".$sqlFile;      // Shell command to create the .sql file for MySQl
-  $createZip = "tar -cvzf $backupFile $sqlFile";  // Shell command to create the backup file we upload to Dropbox
+  /*
+   * Shell commands for generating the .sql and .tgz files
+   */
+  $shell_sql = "mysqldump -h ".DB_HOST ." -u ". DB_USER ." --password=". DB_PASS ." ". DB_NAME ." --> ". TMP_DIR . $file_sql; # create the mysql file
+  $shell_tgz = "tar -cvzf ". TMP_DIR . $file_tgz ." ". TMP_DIR . $file_sql;  // Shell command to create the backup file we upload to Dropbox
+  $shell_rmv = "rm -f ". TMP_DIR . $file_sql;
 
+  /*
+   * Try and execute commands
+   */
   try {
-          exec($createBackup);    // Run shell command to create the .sql file
-          exec($createZip);       // Run shell command to create the .tgz file
+    exec($shell_sql); // Run shell command to create the .sql file
+    exec($shell_tgz); // Run shell command to create the .tgz file
+    exec($shell_rmv); // Run shell command to remove the .sql file
   } catch (Exception $e) {
-          echo "Failed to create CRM DB Backup or ZIP: " . $e->getMessage() . "\n";
+    echo "Failed to create CRM DB Backup or ZIP: " . $e->getMessage() . "\n";
   }
+?>
